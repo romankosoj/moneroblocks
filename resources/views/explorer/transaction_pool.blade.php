401c4aa6e6f2077fc7890d370b4b0ddc0ac02d81
papa_lazzarou <papa_lazzarou@yahoo.com>
2017-03-10 16:32:44 +0000

    change the columns display in the mempool section

--- resources/views/explorer/transaction_pool.blade.php
+++ resources/views/explorer/transaction_pool.blade.php
@@ -17,28 +17,25 @@
 			<div class="panel panel-default panel-table">
 				<div class="panel-heading">
 					<div class="row">
-						<div class="col-xs-2 col-sm-5 col-md-7 hash-header">Hash</div>
-						<div class="col-xs-7 col-sm-5 col-md-3">Output total</div>
-						<div class="col-xs-3 col-sm-2 col-md-2">Fee</div>
+						<div class="col-xs-12 col-sm-5 col-md-6 hash-header">Hash</div>
+						<div class="col-xs-5 col-sm-5 col-md-3">Date Received</div>
+						<div class="col-xs-4 col-sm-2 col-md-2">Fee</div>
+						<div class="col-xs-3 col-sm-2 col-md-1">Size</div>
 					</div>
 				</div>
 				<!-- /.panel-heading -->
 
-				<div id="transaction-pool" class="panel-body">        
+				<div id="transaction-pool" class="panel-body">
 
 					@forelse ($transaction_pool as $transaction)
-					<?php $transaction_json = json_decode($transaction->tx_json); ?>
 						<div class="row show-grid top-row">
-							<div class="col-xs-12 col-sm-12 col-md-7">{{ $transaction->id_hash }}</div>
-							<div class="col-xs-12 col-sm-7 col-md-5 pull-right">
-								@if ($transaction_json->version == 2)
-								<div class="col-xs-7 col-md-6"><i class="fa fa-envelope-o"></i>&nbsp;confidential</div>
-								@else
-								<div class="col-xs-7 col-md-6">@coin($transaction_json->amount)</div>
-								@endif
-								<div class="col-xs-5 col-md-6">@coin($transaction->fee)</div>
+							<div class="col-xs-12 col-sm-12 col-md-6">{{ $transaction->id_hash }}</div>
+							<div class="col-xs-12 col-sm-7 col-md-6 pull-right">
+								<div class="col-xs-5 col-md-5">@datetime($transaction->receive_time)</div>
+								<div class="col-xs-4 col-md-5">@coin($transaction->fee)</div>
+								<div class="col-xs-3 col-md-2">{{ $transaction->blob_size }}</div>
 							</div>
-					</div>
+						</div>
 					@empty
 					<div class="row show-grid top-row">
 						<div class="col-xs-12">No transactions</div>
